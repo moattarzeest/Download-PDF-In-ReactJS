7d39a4e861140e85e58e01c9c8149ff5805dbf1e
Moattar Zeest Rana <90420556+moattarzeest@users.noreply.github.com>
2023-09-04 11:23:31 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+
+function PdfDownloader() {
+  const pdfUrl = "https://arxiv.org/pdf/1308.4675.pdf";
+  const [pdfBlob, setPdfBlob] = useState(null);
+
+  useEffect(() => {
+    // Fetch the PDF file
+    fetch(pdfUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        // Store the PDF blob in state
+        setPdfBlob(blob);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleDownload = () => {
+    if (pdfBlob) {
+      // Create a URL object from the binary data
+      const url = URL.createObjectURL(pdfBlob);
+
+      // Create a temporary link element to download the PDF
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "example.pdf";
+
+      // Add the link to the document and click it
+      document.body.appendChild(link);
+      link.click();
+
+      // Clean up the URL object
+      URL.revokeObjectURL(url);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleDownload}>Download PDF</button>
+    </div>
+  );
+}
+
+export default PdfDownloader;
