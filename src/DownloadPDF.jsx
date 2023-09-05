24fbf5263478fbe4a5aaa4407ebac8053487f8dc
Moattar Zeest Rana <90420556+moattarzeest@users.noreply.github.com>
2023-09-05 14:50:04 +0500

    Create DownloadPDF.jsx

---
+++ src/DownloadPDF.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+
+export default function DownloadPDF()
+{
+    const pdfUrl = "https://arxiv.org/pdf/1308.4675.pdf";
+const [pdfBlob, setPdfBlob] = useState(null);
+
+  useEffect(() => {
+    fetch(pdfUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        setPdfBlob(blob);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleDownload = () => {
+    if (pdfBlob) {
+      const url = URL.createObjectURL(pdfBlob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "example.pdf";
+      document.body.appendChild(link);
+      link.click();
+      URL.revokeObjectURL(url);
+    }
+  };
+
+}
