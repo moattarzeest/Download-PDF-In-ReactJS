e6b6be950656e4972ed2eaf781c7a6e35fc6c45f
Moattar Zeest Rana <90420556+moattarzeest@users.noreply.github.com>
2023-09-05 14:49:37 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
 import React, { useEffect, useState } from "react";
+import DownloadPDF from './DownloadPDF'
 
 function PdfDownloader() {
-  const pdfUrl = "https://arxiv.org/pdf/1308.4675.pdf";
-  const [pdfBlob, setPdfBlob] = useState(null);
 
-  useEffect(() => {
-    // Fetch the PDF file
-    fetch(pdfUrl)
-      .then((response) => response.blob())
-      .then((blob) => {
-        // Store the PDF blob in state
-        setPdfBlob(blob);
-      })
-      .catch((error) => console.error(error));
-  }, []);
-
-  const handleDownload = () => {
-    if (pdfBlob) {
-      // Create a URL object from the binary data
-      const url = URL.createObjectURL(pdfBlob);
-
-      // Create a temporary link element to download the PDF
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = "example.pdf";
-
-      // Add the link to the document and click it
-      document.body.appendChild(link);
-      link.click();
-
-      // Clean up the URL object
-      URL.revokeObjectURL(url);
-    }
-  };
 
   return (
     <div className="myApp">
       <h1>Learn how to download PDF on button click</h1>
       <h3>Click the button below</h3>
-      <button onClick={handleDownload} className="downloadButton">Download PDF</button>
+      <button onClick={<DownloadPDF/>} className="downloadButton">Download PDF</button>
     </div>
   );
 }
